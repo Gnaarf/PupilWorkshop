a0e24b9a6a279d333885ac81cdb73cb729047d95
Gnaarf <gnaarf@gmx.de>
2016-10-25 18:06:40 +0200

    more setup

---
+++ PupilWorkshop/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.position += Vector3.right;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            transform.position += Vector3.right;
+    }
+}
